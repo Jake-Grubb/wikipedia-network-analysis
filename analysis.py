b4e82835ae7f17ab168a12bf43aeca7bda6e880c
Brady Smith <bradsmi@siue.edu>
2019-04-16 16:28:13 -0500

    added analysis.py

---
+++ analysis.py
@@ -0,0 +1,21 @@
+import networkx
+import csv
+import time
+
+#Load the file ./edges.csv using csv module into a python list
+start_time = time.time()
+with open('../../Documents/edges.csv', 'r') as file:
+	reader = csv.reader(file)
+	print("Reader Made in " + str(time.time() - start_time) + " seconds")
+	edgeList = list(reader)
+	print("EdgeList Loaded in " + str(time.time() - start_time) + " seconds")
+#Load the python list of edges into a networkx graph
+graphyboi = networkx.Graph()
+print("Graphy Boi Made in " + str(time.time() - start_time) + " seconds")
+#graphyboi.add_edges_from(edgeList)
+for edge in edgeList:
+	try:
+		graphyboi.add_edge(edge[0], edge[1])
+	except:
+		print(str(edge))
+print("Graphy Boi Added All His Edges in " + str(time.time() - start_time) + " seconds")
