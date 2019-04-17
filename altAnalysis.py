35e2c919704e88ca27046d5f32879a2e1728d97c
Brady Smith <bradsmi@siue.edu>
2019-04-17 11:06:26 -0500

    created an alternate analysis program which loads in edges a different way

---
+++ altAnalysis.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import csv
+import time
+
+graphyboi = nx.Graph()
+start_time = time.time()
+
+with open('../../Documents/edges.csv', 'r') as file:
+	reader = csv.reader(file)
+	for row in reader:
+		try:
+			graphyboi.add_edge(row[0], row[1])
+		except:
+			print(str(row))
+
+print("Graphy Boi Added All His Edges in " + str(time.time() - start_time) + " seconds")
